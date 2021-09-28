5988cb27a6758d792cb5d22074619b342fa40765
Aclecio Cruz <aclecioscruz@gmail.com>
2021-09-28 17:34:39 -0300

    Primeiro commit

---
+++ src/application/Main.java
@@ -0,0 +1,9 @@
+package application;
+import model.entity.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department (1, "Books");
+        System.out.println (obj);
+    }
+}
